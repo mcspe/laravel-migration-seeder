563e4aa4c63eba86d13264b4c1a6112280310039
mcspe <mcspezzaferro@gmail.com>
2023-06-08 15:39:40 +0200

    [add] create model Train

--- database/migrations/2023_06_08_123439_create_trains_table.php
+++ database/migrations/2023_06_08_123439_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_city', 100);
+            $table->string('arrival_city', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10)->unique();
+            $table->tinyInteger('train_car')->unsigned();
+            $table->tinyInteger('on_time')->default(1);
+            $table->tinyInteger('cancelled')->default(0);
             $table->timestamps();
         });
     }
