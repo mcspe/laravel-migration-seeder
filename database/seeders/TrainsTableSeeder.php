8c050d112c216433f1ebe446666e208803e98830
mcspe <mcspezzaferro@gmail.com>
2023-06-08 17:17:59 +0200

    [add] popolate table with faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +16,22 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 15; $i++) {
+            $new_train = new Train();
+            $new_train->company = $faker->word();
+            $new_train->departure_city = $faker->city();
+            $new_train->arrival_city = $faker->city();
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_code = $faker->regexify('[A-Z]{6}[0-4]{4}');
+            $new_train->train_car = $faker->numberBetween(7, 12);
+            $new_train->cancelled = $faker->numberBetween(0, 1);
+            if(!$new_train->cancelled) {
+                $new_train->on_time = $faker->numberBetween(0, 1);
+            }
+            $new_train->save();
+        }
     }
 }
